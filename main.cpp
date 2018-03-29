69629b30a2d4bc586712c0e549a54b107c957410
gcaballero1 <gcaballero1@mail.csuchico.edu>
2018-03-29 11:27:14 -0700

    File Reader

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-//Author:
+//Author: Gabriel Caballero
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -8,15 +9,28 @@ int main()
 {
   ifstream in("example.txt");
   char first = ' ', last = ' ', current;
+  vector<char> list;
 
   //Read from the file opened by in, and the first letter (either upper or
+  while ( !in.eof() )
+  {
+        in>>current;
+        if ( (current>='A' && current>='Z') || (current>='a' && current>='z') )
+        {
+                list.push_back(current);
+        }
+        
+        for ( int i=0; i<list.size(); i++ )
+        {
+                first=list.at(0);
+                last=list.at(list.size()-1);
+        }
   //lower case) should be assigned to first and the last letter (again in
   //either case) from the file to the variable last
-
-
-
-  //Then, finally, this displays the censored version:
-  cout<<first<<"**"<<last<<endl;
+  }
+    in.close();
+    //Then, finally, this displays the censored version:
+    cout<<first<<"**"<<last<<endl;
 
   return 0;
-}
+} //end main
